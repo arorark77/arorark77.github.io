ddc497569bb68cb3f6d90b3ea92c01544cbbdf12
Rakesh <arorark77@gmail.com>
2020-10-25 02:32:46 -0400

    updated script file and added comments

--- 05-Homework/script.js
+++ 05-Homework/script.js
@@ -1,5 +1,5 @@
-// Declare a var for the Wday object
-let workDay = {
+// declare an abject variable to hold hrs of the day
+let dayhrs = {
   "8 AM": "",
   "9 AM": "",
   "10 AM": "",
@@ -11,55 +11,56 @@ let workDay = {
   "4 PM": "",
   "5 PM": "",
 };
+
 // prevent html to load before JS
 $(document).ready(function(){
-  // if nothing in localstorage
-  if(!localStorage.getItem('workDay')) {
-    //update calendar tasks with what is in local storage
-    updateCalendarTasks(workDay);
+  // if no dayhrs object in localstorage
+  if(!localStorage.getItem('dayhrs')) {    
+    //update text area for each hr
+    updateTasks(dayhrs);
   } else {
-    //update caledar rows with what is in local storage
-    updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+    //get text area items from local storage
+    updateTasks(JSON.parse(localStorage.getItem('dayhrs')));
   }
 })
 
-// update date in heading
-$('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+// update current date and time in heading
+$("#currentDay").text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
 
 let counter = 1;
-for(const property in workDay) {
-  console.log(property);
-  let textEntry = "#text-entry" + counter;
-  console.log(textEntry);
-  $(textEntry).text(workDay[property]);
-  let timeId = "#time" + counter;
-  console.log(timeId);
-  let presentHour = moment().hour();
-  console.log(presentHour);
-  let timeString = $(timeId).text();
-  console.log(timeString);
-  let timeNumber = hourNumberFromHourString(timeString);
-  console.log(timeNumber);  
-  if(timeNumber < presentHour) {
-    $(textEntry).addClass("past-hour");
-  } else if (timeNumber > presentHour) {
-    $(textEntry).addClass("future-hour");
+for(const hrs in dayhrs) { 
+  // for each hr in the dayhrs object 
+  let textblock = "#text-entry" + counter;  
+  $(textblock).text(dayhrs[hrs]);
+  let hrId = "#time" + counter;  
+  let presentHour = moment().hour();  
+  let hrString = $(hrId).text();  
+  let hrNumber = hourNumber(hrString);
+
+  // change the color of hr based on past, present or future
+   
+  if(hrNumber < presentHour) {
+    $(textblock).addClass("past-hour");
+  } else if (hrNumber > presentHour) {
+    $(textblock).addClass("future-hour");
   } else {
-    $(textEntry).addClass("present-hour");
+    $(textblock).addClass("present-hour");
   }
   counter ++;
 }
 
+// on save button click, save hr value and corresponding text
+
 $("button").click(function() {
   value = $(this).siblings("textarea").val();
-  console.log(value);
-  hourString = $(this).siblings("div").text();
-  console.log(hourString);
+  
+  hourString = $(this).siblings("div").text();  
   
   saveSchedule(hourString, value);
 });
 
-function hourNumberFromHourString(hourString) {
+function hourNumber(hourString) {
   switch(hourString) {
     case "8 AM": return 8;
     case "9 AM": return 9;
@@ -74,39 +75,43 @@ function hourNumberFromHourString(hourString) {
   }
 }
 
+// load correct dataset
+
 function loadCorrectDataset() {
-  result = localStorage.getItem('workDay')
-  console.log(result);
-  return (result ? result : workDay);
+  result = localStorage.getItem('dayhrs')  
+  return (result ? result : dayhrs);
 }
+
+// Initialize local storage
 
 function initializeLocalStorage() {
-  localStorage.setItem('workDay', JSON.stringify(workDay));
+  localStorage.setItem('dayhrs', JSON.stringify(dayhrs));
 };
 
+// save to local storage
+
 function saveToLocalStorage(dayObj) {
-  localStorage.setItem('workDay', JSON.stringify(dayObj));
+  localStorage.setItem('dayhrs', JSON.stringify(dayObj));
 }
 
+// function to save to local storage
+
 function saveSchedule(hourString, val) {
-  if(!localStorage.getItem('workDay')) {
+  if(!localStorage.getItem('dayhrs')) {
     initializeLocalStorage();
   }
 
-  let workHours = JSON.parse(localStorage.getItem('workDay'));
+  let workHours = JSON.parse(localStorage.getItem('dayhrs'));
   workHours[hourString] = val
 
   saveToLocalStorage(workHours);
 }
 
-function updateCalendarTasks(dayObject) {
+// Function to update text area
+
+function updateTasks(dayObject) {
   $(".calendar-row").each(function(index) {
     let res = $(this).children("div");
     $(this).children("textarea").text(dayObject[res.text()]);
   })
 }
-
-//define workday
-// check if entry exist in local stoage, if yes load it
-// color code the text blocks
-// when save is clicked, store the calendat in local storage
