6754c7e4ef12350c4644f7bce573edd8ceca15e3
Rakesh <arorark77@gmail.com>
2021-01-02 02:33:26 -0500

    eat da burger initial commit

---
+++ burger/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big_Mac');
+INSERT INTO burgers (name) VALUES ('Beyond_Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Veggi', true);
+INSERT INTO burgers (name, devoured) VALUES ('Farm', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken', true);
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
